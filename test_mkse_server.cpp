ffc71d206f3bc930341704748bf14d69f3d891df
Vic-HaoXu <xuhaovic@gmail.com>
2020-08-16 13:50:35 +0800

    fix port

--- test_mkse_server.cpp
+++ test_mkse_server.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
     builder.SetMaxReceiveMessageSize(1024 * 1024 * 1024);
     builder.SetMaxSendMessageSize(1024 * 1024 * 1024);
     std::cout<<"set succeed"<<std::endl;
-    builder.AddListeningPort("0.0.0.0:4241", grpc::InsecureServerCredentials());
+    builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
     std::cout<<"builder register succeed"<<std::endl;
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
